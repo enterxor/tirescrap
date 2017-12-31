50d5228ec7a516d0d3199765c485bdbc85134cbe
enterxor <enterxor@gmail.com>
2017-12-31 14:57:47 +0300

    Fixed incorrect headers of tsv

--- tirescrap/pipelines.py
+++ tirescrap/pipelines.py
@@ -13,7 +13,7 @@ class TirescrapPipeline(object):
 class TSVWriterPipeline(object):
 
 	def open_spider(self, spider):
-		self.file = open('output.csv', 'w', newline='')
+		self.file = open('output.tsv', 'w', newline='')
 		self.writer = csv.writer(self.file,
 								quotechar='\'',dialect='excel-tab')
 		self.writer.writerow(["MPC",
@@ -22,8 +22,8 @@ class TSVWriterPipeline(object):
 			"Product URL",
 			"Zipcode",
 			"Quantity",
-			"ListPrice",
 			"RawPrice",
+			"ListPrice",
 			"Shipping",
 			"Discount",
 			"AddtoCart"])
